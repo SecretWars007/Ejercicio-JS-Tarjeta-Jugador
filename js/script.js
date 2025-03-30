efe96e1f70c31e740760192382d6a1abb5a61ce1
SecretWars007 <enriquezariasedwingustavo@gmail.com>
2025-03-30 18:43:54 -0400

    Task - Comentarios script.js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,17 @@ const frontal = document.querySelector('.frontal');
 const posterior = document.querySelector('.posterior');
 // Función para voltear la tarjeta
 verDetallesButton.addEventListener('click', function(){
+    // utilizamos la transformacion
+    // rotamos en el eje Y la cara frontal para que se oculte
+    // rotamos en el eje Y la cara posterior para que se visualice
     frontal.style = `transform: rotateY(180deg)`
     posterior.style = `transform: rotateY(0deg)` 
 });
 // Función para volver a la cara frontal
 volverButton.addEventListener('click', function(){
+    // utilizamos la transformacion
+    // rotamos en el eje Y la cara frontal para que se visualice
+    // rotamos en el eje Y la cara posterior para que se oculte
     frontal.style = `transform: rotateY(0deg)`
     posterior.style = `transform: rotateY(180deg)` 
 });
